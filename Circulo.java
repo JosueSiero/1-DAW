0f3d8bee3c5c91bbd14b36bddd12328271224dfe
Josue David Siero Mejia <josuedsm45@educastur.es>
2025-12-02 19:57:13 +0100

    proyectos java adicionales

---
+++ Circulo.java
@@ -0,0 +1,17 @@
+public class Circulo {
+    public static int pi=3;
+    public static int numCirculo;
+    private int radio;
+
+    public Circulo(){
+        numCirculo++;
+    }
+
+    public int getRadio (){
+        return radio;
+    }
+
+    public int getNumCirculo(){
+        return numCirculo;
+    }
+}
